d21c2a4944276027a62ef341b32173f172b4d44c
Guanqing Yan <gyan4@wisc.edu>
2016-09-04 13:51:10 -0500

    [React]rewrite sign-in box using material

--- web/js/sign_in.jsx
+++ web/js/sign_in.jsx
@@ -3,12 +3,27 @@ var React = require('react')
 var SignInBox = React.createClass({
   render: function() {
     return (
-      <div className="sign_in_box">
-        Email<br/>
-        <input type="text" name="sign_in_email" /><br/>
-        Password<br/>
-        <input type="password" name="sign_in_password" /><br/>
-        </div>
+      <div className="card">
+        <form>
+          <div className="card-content">
+            <div className="row">
+              <div className="input-field col s12">
+                <input type="text" id="sign_in_email_field" name="sign_in_email" /><br/>
+                <label htmlFor="sign_in_email_field">Email</label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input type="password" id="sign_in_password_field" name="sign_in_password" /><br/>
+                <label htmlFor="sign_in_password_field">Password</label>
+              </div>
+            </div>
+          </div>
+          <div className="card-action">
+            <button className="btn" type="submit">Sign In</button>
+          </div>
+        </form>
+      </div>
     )
   }
 })
