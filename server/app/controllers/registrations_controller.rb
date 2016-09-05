09b7a0ea664c3675e344684a432fe4619659033e
Guanqing Yan <gyan4@wisc.edu>
2016-09-05 16:17:40 -0500

    [Server]make devise communicate using json

---
+++ server/app/controllers/registrations_controller.rb
@@ -0,0 +1,6 @@
+class RegistrationsController < Devise::RegistrationsController
+  # stop static pages from loading
+  # http://blog.andrewray.me/how-to-set-up-devise-ajax-authentication-with-rails-4-0/
+  clear_respond_to
+  respond_to :json
+end
