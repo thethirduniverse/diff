f5c375619d8a2528da697e0d31695395350cb4c7
Guanqing Yan <gyan4@wisc.edu>
2016-09-09 16:28:48 -0500

    [React]add ajax sign-up/in logic

--- web/js/controllers/account_card_controller.js
+++ web/js/controllers/account_card_controller.js
@@ -1,17 +1,12 @@
 import { connect } from 'react-redux'
 import { accountCardShowSignIn, accountCardShowSignUp } from '../actions'
+import $ from 'jquery'
 
 const AccountCard = require('../components/account_card.jsx')
 
 const mapStateToProps = (state, ownProps) => {
   return {
     visible: state.accountCardReducer.visible,
-    signInClicked: function(data) {
-      console.log(data)
-    },
-    signUpClicked: function(data) {
-      console.log(data)
-    }
   }
 }
 
@@ -22,6 +17,28 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     },
     haveAccountClicked: () => {
       dispatch(accountCardShowSignIn())
+    },
+    signInClicked: (data) => {
+      $.post("/users/sign_in", {user: data})
+        .done((res) => {
+          console.log("sign in succeeded with response:")
+          console.log(res)
+        })
+        .fail((res) => {
+          console.log("sign in failed with response:")
+          console.log(res)
+        })
+    },
+    signUpClicked: (data) => {
+      $.post("/users", {user: data})
+        .done((res) => {
+          console.log("sign up succeeded with response:")
+          console.log(res)
+        })
+        .fail((res) => {
+          console.log("sign up failed with response:")
+          console.log(res)
+        })
     }
   }
 }
