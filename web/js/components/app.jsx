4b0e2660421f716032c158f116ead65b10b7837c
Guanqing Yan <gyan4@wisc.edu>
2016-09-09 20:28:58 -0500

    [React]add icon menu to app bar

--- web/js/components/app.jsx
+++ web/js/components/app.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import AccountCardController from '../controllers/account_card_controller.js'
 
 const App = React.createClass({
@@ -10,8 +14,21 @@ const App = React.createClass({
     return (
       <div>
         <AppBar
-          title="Debatable"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          title = 'Debatable'
+          showMenuIconButton = {false}
+          iconElementRight = {
+            <IconMenu
+              iconButtonElement={
+                <IconButton><MoreVertIcon /></IconButton>
+              }
+              anchorOrigin = {{horizontal: 'right', vertical: 'bottom'}}
+              targetOrigin = {{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText="item 1" />
+              <MenuItem primaryText="item 2" />
+              <MenuItem primaryText="item 3" />
+            </IconMenu>
+          }
         />
         <AccountCardController />
       </div>
