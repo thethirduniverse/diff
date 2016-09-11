26f2e007eb1c359814ddac4a64ded5bd5f687496
Guanqing Yan <gyan4@wisc.edu>
2016-09-11 10:56:14 -0500

    [React]add react-logger

--- web/js/debatable.jsx
+++ web/js/debatable.jsx
@@ -3,12 +3,19 @@ setups()
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 import appReducer from './reducers'
-const store = createStore(appReducer)
+const logger = createLogger()
+const store = createStore(
+  appReducer,
+  applyMiddleware(thunk, promise, logger)
+)
 
 import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
