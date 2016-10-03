f9924fc79fa062e64c8c62620d3bec2120c3c03c
Guanqing Yan <gyan4@wisc.edu>
2016-10-03 13:22:58 -0500

    [Server]set datetime to be the only time zone aware field

--- server/config/application.rb
+++ server/config/application.rb
@@ -23,5 +23,7 @@ module Debatable
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.active_record.time_zone_aware_types = [:datetime]
   end
 end
