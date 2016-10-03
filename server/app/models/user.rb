462fd3304a5c8eba961ed9bbe720b7ef6ad728a9
Guanqing Yan <gyan4@wisc.edu>
2016-10-03 12:59:47 -0500

    [Server]make user confirmable

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   has_many :topics
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923232701) do
+ActiveRecord::Schema.define(version: 20161003163045) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,10 @@ ActiveRecord::Schema.define(version: 20160923232701) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.time     "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
