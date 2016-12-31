ba6cd1a2202333244c56fc27586a57b7eee65154
Guanqing Yan <gyan4@wisc.edu>
2016-12-31 00:20:10 -0600

    [Server]implement method to generate post with version

--- server/app/helpers/diff_helper.rb
+++ server/app/helpers/diff_helper.rb
@@ -8,28 +8,47 @@ module DiffHelper
   class CommandFailedError < StandardError
   end
 
-  # rubocop:disable MethodLength, Metrics/AbcSize
+  class VersionNotFoundError < StandardError
+  end
+
   def create_patch(src, dst)
-    path = '/tmp/diff/edit-diffs'
-    f1 = Tempfile.open('diff-src-', path)
-    f2 = Tempfile.open('diff-dst-', path)
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        f1 = open('src', 'w')
+        f2 = open('dst', 'w')
+        f1.print(src)
+        f1.flush
+        f2.print(dst)
+        f2.flush
 
-    begin
-      f1.print(src)
-      f1.flush
-      f2.print(dst)
-      f2.flush
-
-      patch = `git diff --no-index --no-prefix --unified=2 #{f1.path} #{f2.path}`
-    ensure
-      f1.close
-      f1.unlink
-      f2.close
-      f2.unlink
+        patch = `git diff --no-index --no-prefix --unified=2 src dst`
+        raise ContentNotChangedError if $CHILD_STATUS.exitstatus.zero?
+        raise CommandFailedError unless $CHILD_STATUS.exitstatus == 1
+        return patch
+      end
     end
+  end
 
-    raise ContentNotChangedError if $CHILD_STATUS.exitstatus.zero?
-    raise CommandFailedError unless $CHILD_STATUS.exitstatus == 1
-    patch
+  # rubocop:disable AbcSize
+  def post_with_version(post_id, version)
+    # version starts at 0, to revert to version 0, for example, we need to read one edit
+    count = version + 1
+    edits = Edit.where(post_id: post_id).order(version: :asc).limit(count)
+    raise VersionNotFoundError if edits.count < count
+
+    Dir.mktmpdir do |dir|
+      Dir.chdir(dir) do
+        open('src', 'w') { |f| }
+        edits.each do |edit|
+          open('patch', 'w') { |f| f << edit.patch }
+
+          `patch -p0 < patch`
+
+          raise CommandFailedError unless $CHILD_STATUS.exitstatus.zero?
+        end
+
+        return File.read('src')
+      end
+    end
   end
 end
--- server/test/helpers/diff_helper_test.rb
+++ server/test/helpers/diff_helper_test.rb
@@ -3,22 +3,23 @@ require 'test_helper'
 
 class DiffHelperTest < ActionView::TestCase
   include DiffHelper
+  require_relative 'diff_helper_test_seed_content.rb'
 
+  # rubocop:disable LineLength
   test 'it correctly generates from empty string' do
     src = ''
     dst = '\nbb\nc\nddssdds'
-    patch = create_patch(src, dst).split(/\n/)
-    assert_equal '@@ -0,0 +1 @@', patch[-2]
-    assert_equal '{+\\nbb\\nc\\nddssdds+}', patch[-1]
+    patch = create_patch(src, dst)
+    assert_equal "diff --git src dst\nindex e69de29..551ec3e 100644\n--- src\n+++ dst\n@@ -0,0 +1 @@\n+\\nbb\\nc\\nddssdds\n\\ No newline at end of file\n", patch
   end
 
   test 'it correctly generates diff' do
     src = 'a\nb\nc'
     dst = '\nbb\nc'
-    patch = create_patch(src, dst).split(/\n/)
-    assert_equal '@@ -1 +1 @@', patch[-2]
-    assert_equal '[-a\\nb\\nc-]{+\\nbb\\nc+}', patch[-1]
+    patch = create_patch(src, dst)
+    assert_equal "diff --git src dst\nindex ae7ef23..9af36c4 100644\n--- src\n+++ dst\n@@ -1 +1 @@\n-a\\nb\\nc\n\\ No newline at end of file\n+\\nbb\\nc\n\\ No newline at end of file\n", patch
   end
+  # rubocop:enable LineLength
 
   test 'it detects if content not changed' do
     src = 'a\nb\nc'
@@ -26,4 +27,44 @@ class DiffHelperTest < ActionView::TestCase
       create_patch(src, src)
     end
   end
+
+  # rubocop:disable Metrics/BlockLength
+  test 'it can revert a post to a past version' do
+    post = Post.create(title: 'title', content: DiffHelperTestSeedContent::M4)
+    assert_equal true, post.persisted?
+    assert_equal true, Edit.create(
+      post: post,
+      user: User.first,
+      message: 'message',
+      version: 0,
+      patch: create_patch('', DiffHelperTestSeedContent::M1)
+    ).persisted?
+    assert_equal true, Edit.create(
+      post: post,
+      user: User.first,
+      message: 'message',
+      version: 1,
+      patch: create_patch(DiffHelperTestSeedContent::M1, DiffHelperTestSeedContent::M2)
+    ).persisted?
+    assert_equal true, Edit.create(
+      post: post,
+      user: User.first,
+      message: 'message',
+      version: 2,
+      patch: create_patch(DiffHelperTestSeedContent::M2, DiffHelperTestSeedContent::M3)
+    ).persisted?
+    assert_equal true, Edit.create(
+      post: post,
+      user: User.first,
+      message: 'message',
+      version: 3,
+      patch: create_patch(DiffHelperTestSeedContent::M3, DiffHelperTestSeedContent::M4)
+    ).persisted?
+
+    assert_equal DiffHelperTestSeedContent::M1, post_with_version(post.id, 0)
+    assert_equal DiffHelperTestSeedContent::M2, post_with_version(post.id, 1)
+    assert_equal DiffHelperTestSeedContent::M3, post_with_version(post.id, 2)
+    assert_equal DiffHelperTestSeedContent::M4, post_with_version(post.id, 3)
+  end
+  # rubocop:enable Metrics/BlockLength
 end
