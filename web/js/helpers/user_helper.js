014a5ff6edaf3ed0ad5bb9c78749f21a1e6ed1aa
Guanqing Yan <gyan4@wisc.edu>
2016-12-30 20:57:00 -0600

    [React]display anonymous user when name is empty

--- web/js/helpers/user_helper.js
+++ web/js/helpers/user_helper.js
@@ -1,1 +1,6 @@
-export const nameOfUser = (u) => (u ? u.first_name + ' ' + u.last_name : '')
+export const nameOfUser = (u) => {
+  if (!u || (!u.first_name && !u.last_name)) {
+    return 'Anonymous User'
+  }
+  return u.first_name + ' ' + u.last_name
+}
