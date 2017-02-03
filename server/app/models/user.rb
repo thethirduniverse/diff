c16994ed169362f4cca74dbbe5766afa07a01cd7
Guanqing Yan <gyan4@wisc.edu>
2017-02-03 07:02:26 -0600

    [Server]obfuscate image url

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -9,7 +9,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
-  has_attached_file :avatar, styles: { large: '500x500>', medium: '300x300>', thumb: '40x40>' }, default_url: '/assets/:style/default_avatar.jpg'
+  # rubocop:disable LineLength
+  has_attached_file :avatar,
+                    styles: { large: '500x500>', medium: '300x300>', thumb: '40x40>' },
+                    default_url: '/assets/:style/default_avatar.jpg',
+                    url: '/system/users/avatars/:id_partition/:style/:hash.:extension',
+                    hash_secret: '2c9d02240808810c66136227f822b4685347c7b25698a04f9bdbd951bfc0fc21852f11cbb8e5cf138e2c70896629bb0126b20057a29a52dc77ba1d6a072c8cd7'
+  # rubocop:enable LineLength
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
 
   def send_devise_notification(notification, *args)
