0a0cc5d08d3a90c295f014bf202064cfdbd6c3a6
Guanqing Yan <gyan4@wisc.edu>
2017-02-06 23:11:27 -0600

    [Server]force https in prod

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  force_ssl unless: :allow_http?
+
+  def allow_http?
+    !Rails.env.production?
+  end
 end
