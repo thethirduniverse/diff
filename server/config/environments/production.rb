a0d9c14ce44efd4e6bd87ff58568dd5df3ca2d74
Guanqing Yan <gyan4@wisc.edu>
2017-02-07 16:11:00 -0600

    [EB]configure email and delayed job

--- server/config/environments/production.rb
+++ server/config/environments/production.rb
@@ -63,9 +63,18 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'www.diffapp.org' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'www.diffapp.org',
+    user_name:            ENV['mailer_email'],
+    password:             ENV['mailer_password'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
