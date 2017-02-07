5f0bba9ccc8bdee90494b0eabd3a44c1fe79421a
Guanqing Yan <gyan4@wisc.edu>
2017-02-07 12:23:33 -0600

    [Server]move assets directory to public

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   # rubocop:disable LineLength
   has_attached_file :avatar,
                     styles: { large: '500x500>', medium: '300x300>', thumb: '40x40>' },
-                    default_url: '/assets/:style/default_avatar.jpg',
+                    default_url: '/assets/default_avatar/:style/default_avatar.jpg',
                     url: '/system/users/avatars/:id_partition/:style/:hash.:extension',
                     hash_secret: '2c9d02240808810c66136227f822b4685347c7b25698a04f9bdbd951bfc0fc21852f11cbb8e5cf138e2c70896629bb0126b20057a29a52dc77ba1d6a072c8cd7'
   # rubocop:enable LineLength
