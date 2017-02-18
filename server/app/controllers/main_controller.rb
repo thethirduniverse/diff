e51af71c4b7f3293a63c512d96dfa915b0db985a
Guanqing Yan <gyan4@wisc.edu>
2017-02-18 13:33:20 -0600

    [Server]fix an error that cause @bundle_js_path to be empty in production

--- server/app/controllers/main_controller.rb
+++ server/app/controllers/main_controller.rb
@@ -9,12 +9,18 @@ class MainController < ApplicationController
     def load_bundle_js_path
       File.basename(Dir.glob(Rails.root.join('public', 'bundle-*.js'))[0])
     end
+    attr_accessor :bundle_js_path
   end
 
   @bundle_js_path = load_bundle_js_path
 
   def index
-    @bundle_js_path = self.class.load_bundle_js_path unless Rails.env.production?
+    @bundle_js_path =
+      if Rails.env.production?
+        self.class.bundle_js_path
+      else
+        self.class.load_bundle_js_path
+      end
 
     @bootstrap = {
       user_status: verify_user,
